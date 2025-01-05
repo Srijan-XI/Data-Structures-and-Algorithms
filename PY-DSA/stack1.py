e29fa1a0ef62d56ab41affd54a08c55102ce5cc3
Srijan Kumar <142709398+srijan1110@users.noreply.github.com>
2025-01-05 12:38:46 +0530

    dsah

---
+++ PY-DSA/stack1.py
@@ -0,0 +1,26 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        if not self.is_empty():
+            return self.items.pop()
+        return None
+
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1]
+        return None
+
+# Example usage
+stack = Stack()
+stack.push(1)
+stack.push(2)
+print(stack.pop())  # Output: 2
+print(stack.peek()) # Output: 1
