3e734348f5a509045e091d6239ed2b816a4ff840
Srijan Kumar <142709398+srijan1110@users.noreply.github.com>
2025-03-18 22:40:57 +0530

    Add files via upload

---
+++ CPP/bfsalgo.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <list>
+#include <queue>
+
+using namespace std;
+
+class Graph {
+    int V;
+    list<int>* adj;
+
+public:
+    Graph(int V);
+    void addEdge(int v, int w);
+    void BFS(int s);
+};
+
+Graph::Graph(int V) {
+    this->V = V;
+    adj = new list<int>[V];
+}
+
+void Graph::addEdge(int v, int w) {
+    adj[v].push_back(w);
+}
+
+void Graph::BFS(int s) {
+    bool* visited = new bool[V];
+    for (int i = 0; i < V; i++)
+        visited[i] = false;
+
+    queue<int> queue;
+    visited[s] = true;
+    queue.push(s);
+
+    while (!queue.empty()) {
+        s = queue.front();
+        cout << s << " ";
+        queue.pop();
+
+        for (auto i = adj[s].begin(); i != adj[s].end(); ++i) {
+            if (!visited[*i]) {
+                visited[*i] = true;
+                queue.push(*i);
+            }
+        }
+    }
+}
+
+int main() {
+    Graph g(5);
+    g.addEdge(0, 1);
+    g.addEdge(0, 2);
+    g.addEdge(1, 2);
+    g.addEdge(1, 3);
+    g.addEdge(2, 4);
+
+    cout << "Breadth-First Search starting from vertex 0:"<<endl;
+    g.BFS(0);
+
+    return 0;
+}
